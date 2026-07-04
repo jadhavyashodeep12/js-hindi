816025d7fc552a5582bc1e14779a6e09ce87ac9e
jadhavyashodeep12 <jadhavyashodeep12@gmail.com>
2026-07-04 14:03:41 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=12345
+let accountEmail="yash@gamil.com"
+var accountPassword="12345"
+accountCity="mumbai"
+let accountState;
+
+// accountId=2  // not allowed if it is const
+accountEmail="abcd@gmail.com"
+accountPassword="54321"
+accountCity="pune"
+console.log(accountId);
+/*
+Prefer not to use var
+beacuse of issue in block scope and functional scope.
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all JS node as newer version
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Yashodeep")
+
+let name="Yash"
+let age=18
+let isLoggedIn=false
+// number => 2 to power 53 
+// bigint
+// string=>""
+// boolean => true/false
+// null=> stanfalone value               --type of null is an object.
+// undefined=>
+// symbol=> unique
+
+// object
+
+console.log(typeof age); //number
+console.log(typeof undefined);//undefined
+console.log(typeof null);// object
