60d19ce7afda7e46193ae21006a5559069d1f298
jadhavyashodeep12 <jadhavyashodeep12@gmail.com>
2026-07-04 14:48:51 +0000

    conversion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
-let score ="33ab"
+// let score ="33ab"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber=Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // "33"=>33
 // "33abc"=> NaN
@@ -13,15 +13,46 @@ console.log(valueInNumber);
 // undefined=>NaN
 // true=>1;false=>0
 
-let isLoggedIn=1
-let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn=1
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1=>true,0=>false
 // ""=>false
 // "hitesh"=>true
 
-let someNumber=33
-let stringNumber=String(someNumber);
-console.log(typeof(stringNumber));
-console.log(stringNumber);
+// let someNumber=33
+// let stringNumber=String(someNumber);
+// console.log(typeof(stringNumber));
+// console.log(stringNumber);
+
+// *****************************Operations************************* //
+let value=3
+let negValue= -value
+// console.log(negValue);
+
+let str1="hello"
+let str2=" Yash"
+let str3=str1 + str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log((3+4)*5%3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+// link to learn prefix and postfix:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
