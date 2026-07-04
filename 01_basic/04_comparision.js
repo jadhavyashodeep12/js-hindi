ee4be5255b453f2da1ee0a9e34aa9f3d998fdfbb
jadhavyashodeep12 <jadhavyashodeep12@gmail.com>
2026-07-04 15:25:49 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2=!1);
+
+// console.log("2">1);   //automatically converts string into number
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined>=0);
+console.log(undefined==0);
+
+// === (strictly check):
+console.log("2"=== 2);
