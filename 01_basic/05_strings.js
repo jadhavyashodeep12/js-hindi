547dad3d1067d7b2f43fd055a3e22e2df9adb922
jadhavyashodeep12 <jadhavyashodeep12@gmail.com>
2026-07-05 02:56:13 +0000

    Strings

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,20 @@
+const name="yash"
+const repocout=50
+
+// console.log(name + repocout+" Value")
+
+console.log(`hello my name is ${name} and my repo count is ${repocout}`);
+
+// another method to declare string :
+const gamename=new String('Yashodeep')
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+// String methods:
+// .substring
+// .length
+// .indexof
+// .slice
+// .trim
+// .replace
+// .include
