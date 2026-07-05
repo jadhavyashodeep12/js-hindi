5a401a5fe428692461f9f99bd8640b5ba22621d4
jadhavyashodeep12 <jadhavyashodeep12@gmail.com>
2026-07-05 06:17:59 +0000

    datesinJS

---
+++ 01_basic/07_datesinJS.js
@@ -0,0 +1,23 @@
+// // Dates
+// let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log( typeof myDate)
+
+// let myCreateDate= new Date(2023,0,23)
+// console.log(myCreateDate)
+// console.log(myCreateDate.toLocaleString())
+
+// let myCreate= new Date(2023,0,23,5,3)
+// console.log(myCreate.toLocaleString())
+
+let myTimeStamp= Date.now()
+console.log(myTimeStamp);
+
+let newdate= new Date()
+console.log(newdate)
+console.log(newdate.getDate())
+console.log(newdate.getMonth())
+console.log(newdate.getDay())
