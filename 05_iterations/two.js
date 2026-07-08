7a926814662f5ba5ce8f3894a54baad9b86cfd6a
jadhavyashodeep12 <jadhavyashodeep12@gmail.com>
2026-07-08 18:57:11 +0000

    while

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// let index=0
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index=index+2
+// }
+
+// let myArray=["flash","batman","superman"]
+// let index=0
+// while(myArray.length>index){
+//     console.log(`super hero ${myArray[index]}`)
+//     index++
+// }
+
+
+// let score=1
+// do{
+//     console.log(`score is ${score}`)
+//     score++
+// }while(score<=10){
+// }
+
+let score=11
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score<=10);                 //here output is 11 beacuse in do while loop fisrt do loop is excuted
+                                   //then it checks the condition.
+
