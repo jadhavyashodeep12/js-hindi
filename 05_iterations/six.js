ab5d883a1d736ca8d476d97d98d78881f055de07
jadhavyashodeep12 <jadhavyashodeep12@gmail.com>
2026-07-09 15:11:44 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,25 @@
+// const coding=["ja","ruby","java","pyhon","cpp"]
+// const language=coding.forEach((item)=>{
+//     console.log(item)
+//     return item
+// })
+// console.log(language)   // in output there is an undefined that means forEach can't retrun values
+
+// const myNums=[1,2,3,4,5,6,7,8,9,10]
+// // using filter:-
+// let newNms= myNums.filter((num)=>{
+//     if(num>4){
+//         return num
+//     }
+// })
+// // console.log(newNms)
+// // using forEach:-
+// const newnum=[]
+// myNums.forEach((num)=>{
+//     if (num>4){
+//         newnum.push(num)
+//     }
+// })
+// console.log(newnum)
+
+
