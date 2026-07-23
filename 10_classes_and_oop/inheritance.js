109e7f644bcd425fd62ce80e3a4a6dd52f4538cb
Yashodeep <jadhavyashodeep12@gmail.com>
2026-07-23 20:37:43 +0530

    inheritance

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,29 @@
+class User{
+    constructor(username){
+        this.username=username
+    }
+    logMe(){
+        console.log(`USERNAME is ${this.username}`)
+    }
+}
+
+class Teacher extends User{
+    constructor(username,email,password){
+        super(username)
+        this.email=email 
+        this.password=password
+    }
+
+    addCourse(){
+        console.log(`A new course ws added by ${this.username}`)
+    }
+}
+const chai= new Teacher("chai","chai@email.com","1234")
+chai.addCourse()
+const masalaChai=new User("masalachai")
+masalaChai.logMe()
+
+console.log(chai===masalaChai)
+console.log(chai===Teacher)
+console.log(chai instanceof Teacher)
+console.log(chai instanceof User)
