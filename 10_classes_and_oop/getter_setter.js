33f7c73563e2bc5af908c1369abfd5fac0dbd755
Yashodeep <jadhavyashodeep12@gmail.com>
2026-07-25 00:00:22 +0530

    getter-setter

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,20 @@
+class User{
+    constructor(email,password){
+        this.email=email
+        this.password=password
+    }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email= value
+    }
+    get password(){
+        return this._password.toUpperCase()
+    }
+    set password(value){
+        this._password=value.toUpperCase()
+    }
+}
+const hitesh= new User("yash@email.com","abc")
+console.log(hitesh.email)
